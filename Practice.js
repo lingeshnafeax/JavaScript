b34bdbe0983a8e26d38a771f1c4136c3b0207631
lingesh <wlingesh260@gmail.com>
2022-09-11 21:06:33 +0530

    day 2 end

--- Practice.js
+++ Practice.js
@@ -1,20 +1,4 @@
-let grade="L";
-switch (grade){
-    case "A":
-        console.log("You did very well");
-        break;
-    case "B":
-        console.log("You did good");
-        break;
-    case "C":
-        console.log("You did okay");
-        break;
-    case "D":
-        console.log("You barely passed");
-        break;
-    case "E":
-        console.log("You have failed fuckerrrrr !!!!");
-        break
-    default:
-        console.log("Enter a Proper Grade asshole !!!")
-}
+let username = "Lingesh" ;
+let age = 18 ;
+let text =`Hello ${username}<br> You are ${age} years old`
+document.getElementById("mylabel").innerHTML=text
