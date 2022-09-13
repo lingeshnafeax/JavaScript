49acf7a561732d1a1d5e76b3d0462f585f63a1e0
lingesh <wlingesh260@gmail.com>
2022-09-13 19:23:32 +0530

    day 3 before dinner

--- Practice.js
+++ Practice.js
@@ -1,13 +1,21 @@
-let names=["Lingesh","Nafeax","Ugesh","Derin"];
-console.log(names)
-names[3]="Winship"
-console.log(names)
-names.push("Kagura")
-console.log(names)
-names.pop()
-console.log(names)
-names.unshift("Nari")
-console.log(names)
-names.shift()
-console.log(names)
-
+let cards=["A",1,2,3,4,5,6,7,8,9,10,"C",1,2,3,4,5,6,7,8,9,10,"D",1,2,3,4,5,6,7,8,9,10,"H",1,2,3,4,5,6,7,8,9,10]
+let newcards=[]
+for (let a of cards){
+    if (a!="A"||"C"||"D"||"H")
+    newcards.push(`${a}`)
+}
+shuffle(newcards);
+function shuffle(newcards){
+    let curindex=newcards.length;
+    while (curindex!=0){
+        let temp;
+        let randomindex;
+        curindex-=1
+        randomindex=Math.floor(Math.random()*newcards.length)
+        temp=newcards[curindex]
+        newcards[curindex]=newcards[randomindex]
+        newcards[randomindex]=temp
+    }
+    return newcards
+}
+console.log(...newcards)
